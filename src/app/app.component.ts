ff15e64e7ce8cf6bce9b559d51f851a8d0feab85
CMedinapr3 <ch.rash37@gmail.com>
2021-08-09 17:33:27 -0400

    09/08/2021 My first task my first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular2021';
+//{1:'a',2:'a',3:'a',4:'a',5:'a',6:'a'} convertir a un array y sumar los numeros pares
+  constructor(){
+    const a = {1:'a',2:'a',3:'a',4:'a',5:'a',6:'a'};
+   const b = Object.keys(a).map(n => parseInt(n)).reduce((aux,value ) => {
+     if(value%2 ===0){
+       aux = aux + value;
+     }
+     return aux
+   },0);
+   console.log('resultado', b);
+  
+   
+  //[1,2,3,4,5,6] filtrar los numeros impares y mostrarlos como cadena
+   const tareaFilter = [1,2,3,4,5,6].filter(item => item%2 !== 0).join(',');
+   console.log(tareaFilter);
+
+  }
+
+  
 }
