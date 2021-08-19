21aafe842cc69e710cc1cb0e84caf409d06150bf
CMedinapr3 <ch.rash37@gmail.com>
2021-08-19 17:35:34 -0400

    19/08/2021 ng deep , host, styles order

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { filter, map, delay } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular2021';
