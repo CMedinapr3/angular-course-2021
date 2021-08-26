d7f418c704c22166ad02836542b30b9c900e26bc
CMedinapr3 <ch.rash37@gmail.com>
2021-08-26 18:33:26 -0400

    26/08/2021 query params like observable and static

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -99,6 +99,9 @@ export class AppComponent {
     }
 
     onGoView2FromTS():void{
-      this.router.navigate(['view2', this.id, 'sub', 33333])
-    }
+      this.router.navigate(
+        ['view2', this.id, 'sub', 33333],
+        {queryParams: {name: 'maria', lastName: 'gutierrez'}}
+      )
+        }
 }
--- src/app/view2/view2.component.ts
+++ src/app/view2/view2.component.ts
@@ -14,9 +14,13 @@ export class View2Component implements OnInit {
     
     /*this.router.params.subscribe(p => {
       console.log('PARAMS', p);
-    });*/
-
-    console.log('SNAPSHOT', this.router.snapshot.params)
+      });
+    this.router.queryParams.subscribe(q => {
+      console.log('QUERY PARAMS:', q);
+    })
+*/
+console.log('SNAPSHOT', this.router.snapshot.params)
+console.log('SNAPSHOT QP', this.router.snapshot.queryParams)
   }
 
 }
