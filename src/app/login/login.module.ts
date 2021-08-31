2fad17c9b33965c8676cce9decded3c878dd42c7
Christopher Medina Velarde <ch.rash37@gmail.com>
2021-08-31 02:19:56 -0400

    Firebase configuration

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './login.component';
 import { RouterModule, Routes } from '@angular/router';
-
 const routes: Routes = [
   {path: '', component: LoginComponent}
 ]
