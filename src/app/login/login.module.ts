00c665aae1d60df199fdbbeb487b79c38db2fa8f
CMedinapr3 <ch.rash37@gmail.com>
2021-09-01 18:33:01 -0400

    01/09/2021 service with observable

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -9,6 +9,9 @@ import {MatSliderModule} from "@angular/material/slider";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import { Test1Service } from './services/test1.service';
 import {SingletonService} from "./services/singleton.service";
+import { Login1Component } from './components/login1/login1.component';
+import { Login2Component } from './components/login2/login2.component';
+import {UtilsService} from "./services/utils.service";
 
 const routes: Routes = [
   {path: '', component: LoginComponent}
@@ -26,10 +29,13 @@ const routes: Routes = [
   ],
   declarations: [
     LoginComponent,
+    Login1Component,
+    Login2Component
 
   ],
   providers:[
-    Test1Service
+    Test1Service,
+    UtilsService
   ]
 })
 export class LoginModule { }
