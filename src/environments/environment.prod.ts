9b53786c35ed350ae75e81c9623e67c1d577e525
CMedinapr3 <ch.rash37@gmail.com>
2021-09-10 17:06:29 -0400

    fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   app: {
-    apiBaseUrl: "https://facebook-angular-course-default-rtdb.firebaseio.com"
+    apiBaseUrl: "https://facebook-angular-course-10701-default-rtdb.firebaseio.com"
   },
   auth: {
     apiBaseUrl: "https://identitytoolkit.googleapis.com",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   app: {
-    apiBaseUrl: "https://facebook-angular-course-default-rtdb.firebaseio.com"
+    apiBaseUrl: "https://facebook-angular-course-10701-default-rtdb.firebaseio.com"
   },
   auth: {
     apiBaseUrl: "https://identitytoolkit.googleapis.com",
